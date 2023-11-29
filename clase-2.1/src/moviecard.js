167c0b9948082009cbc6f2f3c142fa58b1102c70
Mackaber <Mackaber@users.noreply.github.com>
2023-11-28 21:38:01 -0600

    add moviecard example

---
+++ clase-2.1/src/moviecard.js
@@ -0,0 +1,13 @@
+function MovieCard(img_src, title, description) {
+  return `
+    <div class="card" style="width: 18rem;">
+    <img src="${img_src}" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">${description}</p>
+    </div>
+  </div>
+  `
+}
+
+export default MovieCard
