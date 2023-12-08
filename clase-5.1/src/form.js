7eef49b66a1ccba96b3f0420c3d71881ebb023fc
Mackaber <Mackaber@users.noreply.github.com>
2023-12-07 21:35:03 -0600

    Add form

--- clase-5.1/src/form.js
+++ clase-5.1/src/form.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 function Form() {
 
   document.addEventListener('DOMContentLoaded', (event) => {
@@ -5,8 +7,31 @@ function Form() {
 
     product_form.addEventListener("submit", (event) => {
       event.preventDefault()
+      console.log(event.target.name.value)
+      let product = {
+        name: event.target.name.value,
+        image: event.target.image.value,
+        price: event.target.price.value
+      }
 
-      
+      let data = JSON.stringify(product);
+
+      let config = {
+        method: 'post',
+        url: 'https://crudcrud.com/api/a5b586295151488ab0d8328dc5729388/products',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+
+      axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });  
     })
   })
 
